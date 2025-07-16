2a97c93e46e71e479d0cd66e00029c1f7691a08e
Rohit-Pakhre <pakhrerohit281@gmail.com>
2025-07-16 22:22:51 +0530

    Feat: Form Structure Added.

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,5 +1,169 @@
+import { useState } from "react";
+
 function StudentForm() {
-  return <p>Hello</p>;
+  const [eye, setEye] = useState(false);
+  return (
+    <div className="min-h-screen bg-gray-200 flex flex-col items-center justify-center">
+      <p className="text-2xl uppercase font-medium text-center">
+        Student Details
+      </p>
+      <form className="h-auto p-5 flex flex-col gap-5">
+        {/* Full Name */}
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="border-b-1 p-2 w-100 outline-0"
+        />
+
+        {/* Email Address */}
+        <input
+          type="email"
+          placeholder="E-mail Address"
+          className="border-b-1 p-2 w-100 outline-0"
+        />
+
+        {/* Select Profession */}
+        <div className="relative inline-block w-60">
+          <select
+            name="category"
+            className="w-full appearance-none bg-gray-100 text-gray-700 p-2 rounded-md border border-gray-300 shadow focus:outline-none focus:ring-blue-100 focus:border-blue-400 cursor-pointer"
+          >
+            <option value="">Select Your Profession</option>
+            <option value="Freshman">Freshman</option>
+            <option value="Sophomore">Sophomore</option>
+            <option value="Junior">Junior</option>
+            <option value="Senior">Senior</option>
+          </select>
+
+          {/* Custom arrow icon */}
+          <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-500">
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
+            </svg>
+          </div>
+        </div>
+
+        {/* Select Gender */}
+        <div className="p-1 flex flex-col gap-2">
+          <label className="text-gray-500 font-medium">Select Gender</label>
+          <div className="flex gap-6">
+            {/* Male */}
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="radio"
+                name="gender"
+                value="Male"
+                className="accent-blue-600 w-4 h-4"
+              />
+              <span className="text-gray-700">Male</span>
+            </label>
+
+            {/* Female */}
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="radio"
+                name="gender"
+                value="Female"
+                className="accent-pink-500 w-4 h-4"
+              />
+              <span className="text-gray-700">Female</span>
+            </label>
+
+            {/* Other */}
+            <label className="flex items-center gap-2 cursor-pointer">
+              <input
+                type="radio"
+                name="gender"
+                value="Other"
+                className="accent-gray-500 w-4 h-4"
+              />
+              <span className="text-gray-700">Other</span>
+            </label>
+          </div>
+        </div>
+
+        {/* Password */}
+        <div className="relative border-b-1">
+          <input
+            type={eye ? "text" : "password"}
+            placeholder="Password"
+            className="p-2 w-100 max-w-95 outline-0"
+          />
+          <div
+            className="absolute right-0 top-2 cursor-pointer"
+            onClick={() => setEye(!eye)}
+          >
+            {!eye ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            )}
+          </div>
+        </div>
+
+        {/* Terms and Conditions */}
+        <div className="flex items-center gap-2">
+          <label className="flex items-center gap-2 cursor-pointer text-blue-600">
+            <input
+              type="checkbox"
+              className="w-4 h-4 cursor-pointer accent-blue-600 bg-gray-100"
+            />
+            <span className="underline">Terms & Conditions</span>
+          </label>
+        </div>
+
+        {/* Submit Button */}
+        <div className="flex justify-center items-center p-3 bg-blue-500 shadow-md cursor-pointer rounded-md">
+          <input
+            type="submit"
+            value="Submit"
+            className="font-medium uppercase text-xl text-white"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default StudentForm;
